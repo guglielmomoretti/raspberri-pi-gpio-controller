b289aa177977572f10595b9138072e103fb2aa0d
Guglielmo <guglielmo.moretti@gmail.com>
2014-05-11 19:05:08 +0200

    refactoring

--- src/org/raspberry/client/container/IRaspberryHeader.java
+++
@@ -1,9 +0,0 @@
-package org.raspberry.client.container;
-
-public interface IRaspberryHeader {
-
-	public void setContent(IPiHeaderLayout layout);
-	public IPiHeaderLayout getContent();
-	
-	
-}
--- src/org/raspberry/client/gpiopin/GpioPinState.java
+++
@@ -1,7 +0,0 @@
-package org.raspberry.client.gpiopin;
-
-public enum GpioPinState {
-
-	ON, OFF
-
-}
